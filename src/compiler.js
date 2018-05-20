ab769c3cc4090cd792a13e158581eb7d0cdecc22
Andrew Reeves <areeves42@gmail.com>
2018-05-20 15:37:21 -0400

    Add simple compiler to process the output from the parser

---
+++ src/compiler.js
@@ -0,0 +1,55 @@
+/**
+ * Compile parsed patterns into FSMs
+ */
+class Compiler {
+  /**
+   * Constructor
+   * @param {class}  Inject FSM class dependency
+   */
+  constructor (FSM) {
+    this.FSM = FSM
+  }
+
+  /**
+   * Compile parse tree
+   * @param {object|string} node Node or string literal from the parser
+   * @returns {object}
+   */
+  compile (node) {
+    // string node
+    if (typeof node === 'string') {
+      return this.FSM.fromString(node)
+    }
+
+    // concat node
+    if (Array.isArray(node.concat) && node.concat.length) {
+      let elems = node.concat.slice()
+      let res = this.compile(elems.shift())
+      while (elems.length) {
+        res = this.FSM.concat(
+          res,
+          this.compile(elems.shift())
+        )
+      }
+      return res
+    }
+
+    // union node
+    if (Array.isArray(node.union) && node.union.length) {
+      let elems = node.union.slice()
+      let res = this.compile(elems.shift())
+      while (elems.length) {
+        res = this.FSM.union(
+          res,
+          this.compile(elems.shift())
+        )
+      }
+      return res
+    }
+
+    // unrecognized node
+    throw new Error('Unrecognized node structure' + JSON.stringify(node, null, 2))
+  }
+}
+
+module.exports = Compiler
---
+++ src/compiler.spec.js
@@ -0,0 +1,65 @@
+const expect = require('chai').expect
+
+const Compiler = require('./compiler')
+const FSM = require('./fsm')
+
+/*
+ * These are mostly integration tests insteae of unit tests.
+ * @todo Use injection to spy/stub/mock to get rid of the dependence on the FSM class
+ */
+describe('The Compiler class', () => {
+  it('should be constructed with FSM dependency', () => {
+    let compiler = new Compiler(FSM)
+    expect(compiler).to.be.instanceof(Compiler)
+  })
+
+  describe('The compile() method', () => {
+    it('should be able to compile a string', () => {
+      let compiler = new Compiler(FSM)
+      let target = new FSM()
+      target.add(1, 'f', 2)
+      target.add(2, 'o', 3)
+      target.add(3, 'o', 4)
+      target.startState = 1
+      target.finalStates.add(4)
+      let result = compiler.compile('foo')
+      expect(result).to.deep.equal(target)
+    })
+
+    it('should be able to compile a concat node of three strings', () => {
+      let compiler = new Compiler(FSM)
+      let target = new FSM()
+      target.add(1, 'f', 2)
+      target.add(2, 'o', 3)
+      target.add(3, 'o', 4)
+      target.add(4, 'b', 5)
+      target.add(5, 'a', 6)
+      target.add(6, 'r', 7)
+      target.add(7, 'b', 8)
+      target.add(8, 'a', 9)
+      target.add(9, 'z', 10)
+      target.startState = 1
+      target.finalStates.add(10)
+      let result = compiler.compile({ concat: ['foo', 'bar', 'baz' ] })
+      expect(result).to.deep.equal(target)
+    })
+
+    it('should be able to compile a union node of three strings', () => {
+      let compiler = new Compiler(FSM)
+      let target = new FSM()
+      target.add(1, 'f', 2)
+      target.add(2, 'o', 3)
+      target.add(3, 'o', 4)
+      target.add(1, 'b', 5)
+      target.add(5, 'a', 6)
+      target.add(6, 'r', 7)
+      target.add(1, 'b', 8)
+      target.add(8, 'a', 9)
+      target.add(9, 'z', 10)
+      target.startState = 1
+      target.finalStates.add(4).add(7).add(10)
+      let result = compiler.compile({ union: ['foo', 'bar', 'baz' ] })
+      expect(result).to.deep.equal(target)
+    })
+  })
+})
