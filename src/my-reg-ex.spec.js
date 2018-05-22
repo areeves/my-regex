9ebc828415dc19cbc2abcdebb6e25cd7d34b7bcd
Andrew Reeves <areeves42@gmail.com>
2018-05-22 18:38:24 -0400

    Added integration tests for star- @TODO failing case found

--- src/my-reg-ex.spec.js
+++ src/my-reg-ex.spec.js
@@ -7,6 +7,7 @@ describe('The MyRegEx class', () => {
     let regex = null
     describe('Pattern foo(bar|baz)wakka', () => {
       before(() => regex = new MyRegEx('foo(bar|baz)wakka'))
+
       it('should match foobarwakka', () => {
         expect(regex.test('foobarwakka')).to.be.true
       })
@@ -21,6 +22,46 @@ describe('The MyRegEx class', () => {
 
       it('should NOT match foowakka', () => {
         expect(regex.test('foowakka')).to.be.false
+      })
+    })
+
+    describe('Pattern (foo|bar)*', () => {
+      before(() => regex = new MyRegEx('(foo|bar)*'))
+
+      it('should match empty string', () => {
+        expect(regex.test('')).to.be.true
+      })
+
+      it('should match `foo`', () => {
+        expect(regex.test('foo')).to.be.true
+      })
+
+      it('should match `bar`', () => {
+        expect(regex.test('bar')).to.be.true
+      })
+
+      it('should match `foofoo`', () => {
+        expect(regex.test('foofoo')).to.be.true
+      })
+
+      it('should match `barbar`', () => {
+        expect(regex.test('barbar')).to.be.true
+      })
+    })
+
+    describe('Pattern (foo|bar|baz)*', () => {
+      before(() => regex = new MyRegEx('(foo|bar|baz)*'))
+
+      it('should match `foo`', () => {
+        expect(regex.test('foo')).to.be.true
+      })
+
+      it('should match `baz`', () => {
+        expect(regex.test('baz')).to.be.true
+      })
+
+      it('should match `bazfoo`', () => {
+        expect(regex.test('bazfoo')).to.be.true
       })
     })
   })
